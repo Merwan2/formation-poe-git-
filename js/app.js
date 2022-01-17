0e74c8c1a2c3f23a53bd327bf1581ce284131b61
Merwan Barkallah <merwan.barkallah@gmail.com>
2022-01-17 14:12:31 +0100

    Refactor JS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function additionner(nombre1,nombre2){
-    return nombre1 + nombre 2;
-}
+    return nombre1 + nombre 2;}
 
 function diviser (nombre1, nombre2) {
-    return nombre1 / nombre2
-}
+    return nombre1 / nombre2}
 
 function soustraire (nombre1,nombre2) {
-    return nombre1 - nombre 2
+    return nombre1 - nombre 2}
+function multiplier (nombre1,nombre2) {
+    return nombre1 * nombre 2
 }
